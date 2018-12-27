cd48c0b127af90fa13fea9a3e74e9ce372b67866
subhmaharana <subhransumaharana@gmail.com>
2018-12-28 02:16:24 +0530

    Async Connection demo - Init

---
+++ src/async-connection-demo/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:4242")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Keeps accepting new connection here
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		n, err := io.Copy(os.Stderr, conn)
+		log.Printf("Copied %d bytes; finished with err = %v", n, err)
+	}
+}
