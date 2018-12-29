d7b5524310ba9b73a63cb1c98579f3f3dd4ad02d
subhmaharana <subhransumaharana@gmail.com>
2018-12-29 23:29:49 +0530

    Adding channels to prevent main func from ending

--- src/misc/simple-goroutine-demostration/main.go
+++ src/misc/simple-goroutine-demostration/main.go
@@ -6,18 +6,25 @@ import (
 )
 
 func main() {
-	go conductSportsEvent("Kho Kho", 10)
-	go conductSportsEvent("Kabbadi", 6)
-	go conductSportsEvent("BasketBall", 3)
+	allEventsDone := make(chan int)
+
+	go conductSportsEvent("Kho Kho", 10, allEventsDone)
+	go conductSportsEvent("Kabbadi", 6, allEventsDone)
+	go conductSportsEvent("BasketBall", 3, allEventsDone)
 
 	fmt.Println("End of main function...")
 
+	for i := 1; i <= 3; i++ {
+		fmt.Println(<-allEventsDone)
+	}
+
 	//Adding a sleep here to prevent exit of the program...
-	time.Sleep(time.Duration(10000) * time.Minute)
+	// time.Sleep(time.Duration(10000) * time.Minute)
 }
 
-func conductSportsEvent(sport string, timeInSeconds int64) {
+func conductSportsEvent(sport string, timeInSeconds int64, done chan int) {
 	fmt.Println(sport, "started...: Should take ", timeInSeconds, " seconds")
 	time.Sleep(time.Duration(timeInSeconds) * time.Second)
 	fmt.Println("Finished ", sport)
+	done <- int(timeInSeconds)
 }
